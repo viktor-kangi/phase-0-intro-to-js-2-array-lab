d17b287f1134cb30b8f8b86d33eefdfdc2f49536
victor <victor.buthia@student.moringaschool.com>
2022-11-24 12:44:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+var cats = ['Milo','Otis','Garfield'] 
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+ }
+ 
+ function destructivelyPrependCat(name){
+    cats.unshift(name);
+   return cats;
+ }
+ 
+ function destructivelyRemoveLastCat(){
+    cats.pop();
+   return cats
+ }
+ 
+ function destructivelyRemoveFirstCat(){
+    cats.shift();
+   return cats;
+ }
+ 
+ function appendCat(name){
+   var name_1= [...cats,name];
+   return name_1;
+ }
+ 
+ function prependCat(name){
+   var name_2=[name, ...cats];
+   return name_2
+ }
+ 
+ function removeLastCat(){
+   var caats= cats.slice(0,cats.length-1);
+   return caats;
+ }
+ 
+ function removeFirstCat(){
+   var catts= cats.slice(1)
+   return catts
+ }
